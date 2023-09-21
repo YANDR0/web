37a3d6e5634942456b26c4f8dbdc2f155f4fefb2
RODRIGUEZ BARRETO <yael.rodriguez@iteso.mx>
2023-09-21 08:25:14 -0600

    Ya me dieron ganas de bailar un pinche cumbion bien loco

--- Tarea 5/java.js
+++ Tarea 5/java.js
@@ -1,33 +1,6 @@
 
 //ESTO SE HACE GRACIAS A LA SESIÓN 10
 
-/*
-function probarConFlechas(test){
-    let crearAlumno = (expediente, nombre) => {
-        console.log(arguments);
-    }
-    crearAlumno(34, "Luis")
-}
-
-probarConFlechas("HOLA");*/
-
-
-/*
-let strJSON = '{"nombre":"pepe","nacimiento":"2000-11-30T12:00:00.000Z"}';
-let obj2 = JSON.parse(strJSON, function(key, value){
-    if(key=='nacimiento'){
-        return new Date(value);
-    }
-    return value;
-});
-console.log(obj2);
-console.log(obj2.nacimiento.getFullYear());
-*/
-
-
-function crearLibro(ID, autor, anio, fecha) {
-    return new Libro(ID, autor, anio, fecha)
-}
 
 class Libro {
     constructor(ID, autor, anio, fecha){
@@ -38,32 +11,66 @@ class Libro {
     }
 }
 
-let array = []
-
-for (let i = 0; i < 10; i++) {
-    id = i+1;
-    autor = "Autor" + (Math.floor((Math.random() * 100))+1);
-    anio = Math.floor(Math.random()*21) + 2000;
-    fecha = new Date(anio + "-01-01T12:00:00.000Z");
-
-    array.push(crearLibro(id, autor, anio, fecha))
+function crearLibro(ID, autor, anio, fecha) {
+    return new Libro(ID, autor, anio, fecha)
 }
 
-array.unshift(crearLibro(0,"Juan",1999,new Date("1999-01-01T12:00:00.000Z")))
-console.table(array);
+function clone_arr(array){
+    let new_array = []
+    let size = array.length
+    for(let i = 0; i < size; i++){
+        new_array.push(array[i]);
+    }
+
+    return new_array;
+}
+
+function delete_element(id, array){
+
+}
+
+function ejercicio_1(array){
+
+    console.log("Ejercicio 1")
+
+    for (let i = 0; i < 10; i++) {
+        id = i+1;
+        autor = "Autor" + (Math.floor((Math.random() * 100))+1);
+        anio = Math.floor(Math.random()*21) + 2000;
+        fecha = new Date(anio + "-01-01T12:00:00.000Z");
+    
+        array.push(crearLibro(id, autor, anio, fecha))
+    }
+
+    console.table(array);
+
+    let v_json = JSON.parse(JSON.stringify(array))
+    console.log(v_json)
+}
+
+function ejercicio_2(array){
+
+    console.log("\nEjercicio 2");
+    array.unshift(crearLibro(0,"Juan",1999,new Date("1999-01-01T12:00:00.000Z")));
+    let size = array.length
+    let array_2 = []
+    let new_array = clone_arr(array)
+
+    for(let i = 0; i < size; i++){
+        let tmp = new_array.pop();
+        array_2.push(tmp);
+    }
+
+    console.table(array_2);
+}
+
+function ejercicio_3(array){
+    
+}
+
+
 
 /*
-let array_2 = []
-let size = array.length
-
-for(let i = 0; i < size; i++){
-    let tmp = array.pop();
-    array_2.push(tmp);
-}
-
-console.table(array);
-console.table(array_2);
-*/
 
 let copia = array.concat();
 
@@ -75,9 +82,9 @@ function del (iden, arreglo){
 //find
 del(3, copia);
 console.table(copia);
+*/
 
-
-//let v_json = JSON.parse(JSON.stringify(array));
+//;
 //console.log(v_json);
 
 //unshift meter al principio
@@ -87,9 +94,10 @@ console.table(copia);
 
 
 
+let array = []
 
-
-
+ejercicio_1(array)
+ejercicio_2(array)
 
 
 
